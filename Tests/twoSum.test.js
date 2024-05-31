99b58e54a80b9a797b833f7a50b776be75550c2c
Tako <tako520cn@gmail.com>
2024-05-31 17:04:11 -0400

    update

--- Tests/twoSum.test.js
+++ Tests/twoSum.test.js
@@ -1,5 +1,19 @@
 const twoSum = require('../Solutions/twoSum')
 
-test('nums: [2,7,11,15], target: 9', ()=>{
-  expect(twoSum([2,7,11,15], 9)).toEqual([0,1]);
+test('nums: [2,7,11,15] success', ()=>{
+  const arr = [2,7,11,15]
+  expect(twoSum(arr, 9)).toEqual([0,1]);
+  expect(twoSum(arr, 26)).toEqual([2,3]);
+})
+
+test('nums: [-1,-2,11,15] success', ()=>{
+  const arr = [-1,-2,11,15]
+  expect(twoSum(arr, -3)).toEqual([0,1]);
+  expect(twoSum(arr, 9)).toEqual([1,2]);
+})
+
+test('nums: [] success', ()=>{
+  const arr = []
+  expect(twoSum(arr, -3)).toEqual(null);
+  expect(twoSum(arr, 9)).toEqual([]);
 })
