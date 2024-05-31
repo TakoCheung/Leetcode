824484d22cc757cfefe0d62052c92408c69996d8
Tako <tako520cn@gmail.com>
2024-05-31 19:09:36 -0400

    update

--- Tests/twoSum.test.js
+++ Tests/twoSum.test.js
@@ -11,9 +11,3 @@ test('nums: [-1,-2,11,15] success', ()=>{
   expect(twoSum(arr, -3)).toEqual([0,1]);
   expect(twoSum(arr, 9)).toEqual([1,2]);
 })
-
-test('nums: [] success', ()=>{
-  const arr = []
-  expect(twoSum(arr, -3)).toEqual(null);
-  expect(twoSum(arr, 9)).toEqual([]);
-})
