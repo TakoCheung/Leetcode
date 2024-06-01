8bdc3c026a50b17b8ea8f14e4e05b7d7e4ea06f7
Tako <tako520cn@gmail.com>
2024-06-01 17:07:29 -0400

    update

--- Solutions/letterCombinations.js
+++ Solutions/letterCombinations.js
@@ -1,9 +1,11 @@
+const util = require('../Utils/combinations')
+
 var letterCombinations = function (digits) {
 	if (!digits.length) {
 		return [];
 	}
 
-	const digitToLetters = {
+  util.setN({
 		2: "abc",
 		3: "def",
 		4: "ghi",
@@ -12,26 +14,10 @@ var letterCombinations = function (digits) {
 		7: "pqrs",
 		8: "tuv",
 		9: "wxyz",
-	};
+	})
+  util.setC(digits)
 
-	const res = [];
-
-	function backtrack(idx, comb) {
-		// console.log("1: ", idx, comb);
-		if (idx === digits.length) {
-			res.push(comb);
-			return;
-		}
-
-		for (const letter of digitToLetters[digits[idx]]) {
-			// console.log("2: ", idx, comb, letter);
-			backtrack(idx + 1, comb + letter);
-		}
-	}
-
-	backtrack(0, "");
-
-	return res;
+	return util.backtrack(0, "");
 };
 
 module.exports = letterCombinations;
--- Tests/letterCombinations.test.js
+++ Tests/letterCombinations.test.js
@@ -1,18 +1,18 @@
 const letterCombinations = require('../Solutions/letterCombinations')
 
-test('digit: [2,3]', ()=>{
-  const digit = [2,3]
-  expect(letterCombinations(digit)).toEqual(["ad","ae","af","bd","be","bf","cd","ce","cf"]);
+test('digits: [2,3]', ()=>{
+  const digits = [2,3]
+  expect(letterCombinations(digits)).toEqual(["ad","ae","af","bd","be","bf","cd","ce","cf"]);
 })
 
-test('digit: [2,3,4,5]', ()=>{
-  const digit = [2,3,4,5]
-  expect(letterCombinations(digit)).toEqual(["adgj","adgk","adgl","adhj","adhk","adhl","adij","adik","adil","aegj","aegk","aegl","aehj","aehk","aehl","aeij","aeik","aeil","afgj","afgk","afgl","afhj","afhk","afhl","afij","afik","afil","bdgj","bdgk","bdgl","bdhj","bdhk","bdhl","bdij","bdik","bdil","begj","begk","begl","behj","behk","behl","beij","beik","beil","bfgj","bfgk","bfgl","bfhj","bfhk","bfhl","bfij","bfik","bfil","cdgj","cdgk","cdgl","cdhj","cdhk","cdhl","cdij","cdik","cdil","cegj","cegk","cegl","cehj","cehk","cehl","ceij","ceik","ceil","cfgj","cfgk","cfgl","cfhj","cfhk","cfhl","cfij","cfik","cfil"]);
+test('digits: [2,3,4,5]', ()=>{
+  const digits = [2,3,4,5]
+  expect(letterCombinations(digits)).toEqual(["adgj","adgk","adgl","adhj","adhk","adhl","adij","adik","adil","aegj","aegk","aegl","aehj","aehk","aehl","aeij","aeik","aeil","afgj","afgk","afgl","afhj","afhk","afhl","afij","afik","afil","bdgj","bdgk","bdgl","bdhj","bdhk","bdhl","bdij","bdik","bdil","begj","begk","begl","behj","behk","behl","beij","beik","beil","bfgj","bfgk","bfgl","bfhj","bfhk","bfhl","bfij","bfik","bfil","cdgj","cdgk","cdgl","cdhj","cdhk","cdhl","cdij","cdik","cdil","cegj","cegk","cegl","cehj","cehk","cehl","ceij","ceik","ceil","cfgj","cfgk","cfgl","cfhj","cfhk","cfhl","cfij","cfik","cfil"]);
 })
 
-test('digit: [9]', ()=>{
-  const digit = [9]
-  expect(letterCombinations(digit)).toEqual(["w","x","y","z"]);
+test('digits: [9]', ()=>{
+  const digits = [9]
+  expect(letterCombinations(digits)).toEqual(["w","x","y","z"]);
 })
 
 test('digit: [9]', ()=>{
