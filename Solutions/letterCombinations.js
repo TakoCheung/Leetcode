a80d402c687d0c3127187f7f6d6b4a0e5a8514bb
Tako <tako520cn@gmail.com>
2024-06-01 12:13:49 -0400

    update

--- Solutions/letterCombinations.js
+++ Solutions/letterCombinations.js
@@ -1,36 +1,37 @@
-var letterCombinations = function(digits) {
-  if (!digits.length) {
-      return [];
-  }
-  
-  const digitToLetters = {
-      '2': 'abc',
-      '3': 'def',
-      '4': 'ghi',
-      '5': 'jkl',
-      '6': 'mno',
-      '7': 'pqrs',
-      '8': 'tuv',
-      '9': 'wxyz'
-  };
-  
-  const res = [];
-  
-  function backtrack(idx, comb) {
-      // console.log(idx, comb)
-      if (idx === digits.length) {
-          res.push(comb);
-          return;
-      }
-      
-      for (const letter of digitToLetters[digits[idx]]) {
-          backtrack(idx + 1, comb + letter);
-      }
-  }
-  
-  backtrack(0, "");
-  
-  return res;    
+var letterCombinations = function (digits) {
+	if (!digits.length) {
+		return [];
+	}
+
+	const digitToLetters = {
+		2: "abc",
+		3: "def",
+		4: "ghi",
+		5: "jkl",
+		6: "mno",
+		7: "pqrs",
+		8: "tuv",
+		9: "wxyz",
+	};
+
+	const res = [];
+
+	function backtrack(idx, comb) {
+		// console.log("1: ", idx, comb);
+		if (idx === digits.length) {
+			res.push(comb);
+			return;
+		}
+
+		for (const letter of digitToLetters[digits[idx]]) {
+			// console.log("2: ", idx, comb, letter);
+			backtrack(idx + 1, comb + letter);
+		}
+	}
+
+	backtrack(0, "");
+
+	return res;
 };
 
 module.exports = letterCombinations;
