348ac8ead155c66090d0e7ce62c463c6337c31ea
Tako <tako520cn@gmail.com>
2024-06-03 08:26:08 -0400

    start working on DSA

--- Tests/fourSum.test.js
+++ Tests/fourSum.test.js
@@ -1,6 +1,11 @@
-const twoSum = require('../Solutions/fourSum')
+const fourSum = require('../Solutions/fourSum')
 
 test('nums: [1,0,-1,0,-2,2] | target: 0', ()=>{
   const arr = [1,0,-1,0,-2,2]
-  expect(twoSum(arr, 9)).toEqual([[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]);
+  expect(fourSum(arr, 0)).toEqual(expect.arrayContaining([[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]));
+})
+
+test('nums: [2,2,2,2] | target: 8', ()=>{
+  const arr = [2,2,2,2]
+  expect(fourSum(arr, 8)).toEqual([[2,2,2,2]]);
 })
--- Utils/combinations.js
+++ Utils/combinations.js
@@ -28,8 +28,29 @@ function backtrack(idx, comb) {
   return ret
 }
 
+function getCombinations(arr, k) {
+  let combinations = [];
+  
+  function helper(start, combo) {
+    if (combo.length === k) {
+      combinations.push([...combo]);
+      return;
+    }
+
+    for (let i = start; i < arr.length; i++) {
+      combo.push(arr[i]);
+      helper(i + 1, combo);
+      combo.pop();
+    }
+  }
+
+  helper(0, []);
+  return combinations;
+}
+
 module.exports = {
   backtrack: backtrack,
   setN: setN,
-  setC: setC
+  setC: setC,
+  getCombinations: getCombinations
 };
