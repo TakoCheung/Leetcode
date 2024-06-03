5859d6a3fbcfb761b703ea78916fb5dec006f0e9
Tako <tako520cn@gmail.com>
2024-06-03 08:47:46 -0400

    start woring on DSA

--- Solutions/fourSum.js
+++ Solutions/fourSum.js
@@ -13,24 +13,26 @@ var fourSum = function (nums, target) {
 	}
 	// console.log(ret)
 	return ret.sort((a, b) => a - b);
-	// const results = [];
-	// if (nums.length < 4) return results;
+};
 
+function fourSumFromThreeSum(nums, target){
+	const results = [];
+	if (nums.length < 4) return results;
 	// nums.sort((a, b) => a - b); // Step 1: Sort the array
 	for (let j = 0; j <= nums.length - 4; j++) {
-		for (let i = j + 1; i <= nums.length - 3; i++) {
+		for (let i = j; i <= nums.length - 3; i++) {
 			// Step 2: Fixed pointer
 			// if (i > 0 && nums[i] === nums[i - 1]) continue; // Avoid duplicates
 
-			let left = i + 1;
+			let left = i - 1;
 			let right = nums.length - 1;
 
 			while (left < right) {
 				const sum = nums[i] + nums[left] + nums[right] + nums[j];
 
-				if (sum === 0) {
+				if (sum === target) {
 					// Found a triplet
-					results.push([nums[j], nums[i], nums[left], nums[right]]);
+					results.push( nums[i], nums[left], [nums[j], nums[right]]);
 
 					// Move both pointers to avoid duplicates
 					// while (left < right && nums[left] === nums[left + 1]) left++;
@@ -47,8 +49,10 @@ var fourSum = function (nums, target) {
 			}
 		}
 	}
+}
 
-	return results;
+module.exports = {
+  fourSum: fourSum,
+	fourSumFromThreeSum, fourSumFromThreeSum
 };
 
-module.exports = fourSum;
--- Tests/fourSum.test.js
+++ Tests/fourSum.test.js
@@ -1,11 +1,47 @@
-const fourSum = require('../Solutions/fourSum')
+const fourSum = require("../Solutions/fourSum");
 
-test('nums: [1,0,-1,0,-2,2] | target: 0', ()=>{
-  const arr = [1,0,-1,0,-2,2]
-  expect(fourSum(arr, 0)).toEqual(expect.arrayContaining([[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]));
-})
+describe("fourSum", () => {
+	test("nums: [1,0,-1,0,-2,2] | target: 0", () => {
+		const arr = [1, 0, -1, 0, -2, 2];
+		expect(fourSum.fourSum(arr, 0)).toEqual(
+			expect.arrayContaining([
+				[-2, -1, 1, 2],
+				[-2, 0, 0, 2],
+				[-1, 0, 0, 1],
+			])
+		);
+	});
 
-test('nums: [2,2,2,2] | target: 8', ()=>{
-  const arr = [2,2,2,2]
-  expect(fourSum(arr, 8)).toEqual([[2,2,2,2]]);
-})
+	test("nums: [2,2,2,2] | target: 8", () => {
+		const arr = [2, 2, 2, 2];
+		expect(fourSum.fourSum(arr, 8)).toEqual([[2, 2, 2, 2]]);
+	});
+
+	test("nums: [] | target: 8", () => {
+		const arr = [];
+		expect(fourSum.fourSum(arr, 8)).toEqual([]);
+	});
+});
+
+describe.skip("fourSumFromThreeSum", () => {
+	test("nums: [1,0,-1,0,-2,2] | target: 0", () => {
+		const arr = [1, 0, -1, 0, -2, 2];
+		expect(fourSum.fourSumFromThreeSum(arr, 0)).toEqual(
+			expect.arrayContaining([
+				[-2, -1, 1, 2],
+				[-2, 0, 0, 2],
+				[-1, 0, 0, 1],
+			])
+		);
+	});
+
+	test("nums: [2,2,2,2] | target: 8", () => {
+		const arr = [2, 2, 2, 2];
+		expect(fourSum.fourSumFromThreeSum(arr, 8)).toEqual([[2, 2, 2, 2]]);
+	});
+
+	test("nums: [] | target: 8", () => {
+		const arr = [];
+		expect(fourSum.fourSumFromThreeSum(arr, 8)).toEqual([]);
+	});
+});
