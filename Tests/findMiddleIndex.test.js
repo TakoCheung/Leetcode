6107b0e0d7cb386669689e8683f76e9abb491ce6
Tako <tako520cn@gmail.com>
2024-06-03 15:03:26 -0400

    findMiddleIndex

--- Tests/findMiddleIndex.test.js
+++ Tests/findMiddleIndex.test.js
@@ -1,11 +1,25 @@
-const findMiddleIndex = require('../Solutions/findMiddleIndex')
+const testFunc = require("../Solutions/findMiddleIndex");
 
-test("nums: [2,3,-1,8,4] ", () => {
-  const arr = [2,3,-1,8,4];
-  expect(findMiddleIndex(arr)).toBe(3);
+describe("findMiddleIndex", () => {
+	test("nums: [2,3,-1,8,4] ", () => {
+		const arr = [2, 3, -1, 8, 4];
+		expect(testFunc.findMiddleIndex(arr)).toBe(3);
+	});
+
+	test("nums: [1,-1,4] ", () => {
+		const arr = [1, -1, 4];
+		expect(testFunc.findMiddleIndex(arr)).toBe(2);
+	});
 });
 
-test("nums: [1,-1,4] ", () => {
-  const arr = [1,-1,4];
-  expect(findMiddleIndex(arr)).toBe(2);
+describe.skip("findMiddleIndexWithReduce", () => {
+	test("nums: [2,3,-1,8,4] ", () => {
+		const arr = [2, 3, -1, 8, 4];
+		expect(testFunc.findMiddleIndexWithReduce(arr)).toBe(3);
+	});
+
+	test("nums: [1,-1,4] ", () => {
+		const arr = [1, -1, 4];
+		expect(testFunc.findMiddleIndexWithReduce(arr)).toBe(2);
+	});
 });
